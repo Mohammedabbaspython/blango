c1fd21ba5fe00235a0ea195e26d96ce2b4b70193
Mohamed Abbas <mabas8899@gmail.com>
2025-08-22 17:07:46 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from django.contrib.auth import get_user_model
 from django.utils.html import format_html
+from blog.models import Post
 
 register = template.Library()
 user_model = get_user_model()
@@ -8,6 +9,7 @@ user_model = get_user_model()
 
 @register.filter
 def author_details(author, user=None):
+  
     if not isinstance(author, user_model):
         # return empty string as safe default
         return ""
@@ -28,3 +30,30 @@ def author_details(author, user=None):
         suffix = ""
 
     return format_html("{}{}{}", prefix, name, suffix)
+
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    ctx = {"title": "Recent Posts", "posts": posts}
+    return ctx
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 
@@ -6,3 +6,11 @@ def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     ctx = {"posts": posts}
     return render(request, "blog/index.html", ctx)
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+
+    ctx = {"post": post}
+    
+    return render(request, "blog/post-detail.html", ctx)
